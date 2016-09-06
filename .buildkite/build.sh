c9d3db15586c4f5c7d92dfb2da66be7d6f066e6e
Camilo Rivera <camilo@riverapineda.com>
2016-09-06 13:17:08 -0600

    buildkite setup

---
+++ .buildkite/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -eo pipefail
+echo "--- Set Node Version"
+. "$NVM_DIR/nvm.sh"
+nvm current
+nvm use 4
+echo "--- Install Dependencies"
+npm install -g gulp
+npm install
+npm update
+echo "--- Build And Test"
+npm test
+if [[ "$BUILDKITE_BRANCH" == "develop"  ]]; then
+  echo "--- deploy to develop"
+  gulp deploy
+  buildkite-agent artifact upload "dist/**/*.zip"
+fi
+if [[ "$BUILDKITE_BRANCH" == "staging"  ]]; then
+  echo "--- deploy to staging"
+  gulp deploy
+  buildkite-agent artifact upload "dist/**/*.zip"
+fi
+if [[ "$BUILDKITE_BRANCH" == "master"  ]]; then
+  echo "--- deploy to production"
+  gulp deploy
+  buildkite-agent artifact upload "dist/**/*.zip"
+fi
