6305d5b3a93f6600a1e54a7665f163e8fa2d8561
William Avila <william.zx1@gmail.com>
2016-10-18 15:14:32 -0600

    Adding logs

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -4,29 +4,11 @@ set -eo pipefail
 echo "--- Set Node Version"
 . "$NVM_DIR/nvm.sh"
 nvm current
-nvm use 4
+nvm use 4 | tee -a bk-pipeline.log
 echo "--- Install Dependencies"
-npm install -g gulp
-npm update
+yarn | tee -a bk-pipeline.log
 echo "--- Build"
 if [[ "$BUILDKITE_BRANCH" == "develop"  ]]; then
   export NODE_ENV=dev
 fi
-gulp compile
-echo "--- Test"
-gulp test
-if [[ "$BUILDKITE_BRANCH" == "develop"  ]]; then
-  echo "--- deploy to develop"
-  gulp deploy
-  buildkite-agent artifact upload "zip/**/*.zip"
-fi
-if [[ "$BUILDKITE_BRANCH" == "staging"  ]]; then
-  echo "--- deploy to staging"
-  gulp deploy
-  buildkite-agent artifact upload "zip/**/*.zip"
-fi
-if [[ "$BUILDKITE_BRANCH" == "master"  ]]; then
-  echo "--- deploy to production"
-  gulp deploy
-  buildkite-agent artifact upload "zip/**/*.zip"
-fi
+gulp compile | tee -a bk-pipeline.log
