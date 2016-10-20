9828141a19b46d428fd6b03aef13c5b53de03f9a
William <william@acklenavenue.com>
2016-10-20 15:26:31 -0600

    Update test.sh

--- .buildkite/test.sh
+++ .buildkite/test.sh
@@ -3,6 +3,6 @@
 set -eo pipefail
 echo "--- Set Node Version"
 . "$NVM_DIR/nvm.sh"
-nvm use 4 | tee -a bk-pipeline.log
+nvm use default | tee -a bk-pipeline.log
 echo "--- Test" | tee -a bk-pipeline.log
 gulp test | tee -a bk-pipeline.log
