031f1ef84af9f6e626039a4eb06e632888e8f003
Rene Rosa <realpasro09@hotmail.com>
2016-10-24 14:50:49 -0600

    deploying indigo-backend staging environment

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -5,5 +5,5 @@ echo "--- Set Node Version"
 . "$NVM_DIR/nvm.sh"
 nvm use default
 echo "--- Deploy to $BUILDKITE_BRANCH"
-gulp deploy | tee -a bk-pipeline.log
-buildkite-agent artifact upload "zip/*.zip"
+gulp prepare-deployment | tee -a bk-pipeline.log
+expect ./.buildkite/stagingDeploy.sh | tee -a bk-pipeline.log
