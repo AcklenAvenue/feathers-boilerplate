afb0bbc0a8ab88c3cbeac9e54704949c60b6a43d
Camilo Rivera <camilo@riverapineda.com>
2016-11-01 23:25:46 -0600

    adding new script for new deploy process

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -5,5 +5,7 @@ echo "--- Set Node Version"
 . "$NVM_DIR/nvm.sh"
 nvm use default
 echo "--- Deploy to $BUILDKITE_BRANCH"
-gulp deploy | tee -a bk-pipeline.log
+#gulp deploy | tee -a bk-pipeline.log
+gulp zip-app
+scp -i "~/acklenavenue.pem" indigo-backend-develop.zip centos@ec2-54-162-255-166.compute-1.amazonaws.com:/home/centos/
 buildkite-agent artifact upload "zip/*.zip"
