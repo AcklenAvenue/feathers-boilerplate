455d4f84b8293e5e479b321599b978e6b8caf26e
Camilo Rivera <camilo@riverapineda.com>
2016-11-02 00:01:02 -0600

    test expect scp

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -10,5 +10,5 @@ gulp zip-app
 cd zip
 sudo cp ~/acklenavenue.pem acklenavenue.pem
 sudo chmod 400 acklenavenue.pem
-scp -i "acklenavenue.pem" -o StrictHostKeyChecking=no indigo-backend-develop.zip centos@ec2-54-162-255-166.compute-1.amazonaws.com:/home/centos/
+expect ./.buildkite/devDeploy.sh | tee -a bk-pipeline.log
 #buildkite-agent artifact upload "*.zip"
---
+++ .buildkite/devDeploy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/expect
+    scp -i "acklenavenue.pem" indigo-backend-develop.zip centos@ec2-54-162-255-166.compute-1.amazonaws.com:/home/centos/
+    expect "Are you sure you want to continue connecting (yes/no)?"
+    send yes\n
+    interact
