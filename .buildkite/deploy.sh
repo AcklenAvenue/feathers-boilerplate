70551fcbda7f45a40d5a4d90ad4fb6c879dd2815
Camilo Rivera <camilo@riverapineda.com>
2016-11-01 23:54:48 -0600

    no yes

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -10,5 +10,5 @@ gulp zip-app
 cd zip
 sudo cp ~/acklenavenue.pem acklenavenue.pem
 sudo chmod 400 acklenavenue.pem
-scp -i "acklenavenue.pem" -B indigo-backend-develop.zip centos@ec2-54-162-255-166.compute-1.amazonaws.com:/home/centos/
+scp -i "acklenavenue.pem" -o StrictHostKeyChecking=no indigo-backend-develop.zip centos@ec2-54-162-255-166.compute-1.amazonaws.com:/home/centos/
 #buildkite-agent artifact upload "*.zip"
