02c111ba3c08322de0e17532882f61b692db18b0
Camilo Rivera <camilo@riverapineda.com>
2016-11-02 00:40:10 -0600

    test ssh

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -14,4 +14,10 @@ expect -c "
    spawn sudo scp -i "acklenavenue.pem" indigo-backend-dev.zip centos@ec2-54-162-255-166.compute-1.amazonaws.com:/home/centos/
    expect Are you sure you want to continue connecting (yes/no)? { send yes\n }
 "
+expect -c "
+   spawn sudo ssh -i "acklenavenue.pem" centos@ec2-54-162-255-166.compute-1.amazonaws.com
+   expect Are you sure you want to continue connecting (yes/no)? { send yes\n }
+"
+pwd
+exit
 buildkite-agent artifact upload "*.zip"
