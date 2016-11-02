9f52c0b5976bdc507065cd000438cebc0ac3a21f
Camilo Rivera <camilo@riverapineda.com>
2016-11-02 00:29:18 -0600

    fix spawn

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -11,7 +11,7 @@ cd zip
 sudo cp ~/acklenavenue.pem acklenavenue.pem
 sudo chmod 400 acklenavenue.pem
 expect -c "
-   sudo spawn scp -i "acklenavenue.pem" indigo-backend-develop.zip centos@ec2-54-162-255-166.compute-1.amazonaws.com:/home/centos/
+   spawn sudo scp -i "acklenavenue.pem" indigo-backend-develop.zip centos@ec2-54-162-255-166.compute-1.amazonaws.com:/home/centos/
    expect Are you sure you want to continue connecting (yes/no)? { send yes\n }
    interact
 "
