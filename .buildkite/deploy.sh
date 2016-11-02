f836dc55282e02d7d99f2364f2502dbd328e46af
Camilo Rivera <camilo@riverapineda.com>
2016-11-02 00:49:56 -0600

    test ssh

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -15,7 +15,7 @@ expect -c "
    expect Are you sure you want to continue connecting (yes/no)? { send yes\n }
 "
 expect -c "
-   spawn sudo ssh -i "acklenavenue.pem" centos@ec2-54-162-255-166.compute-1.amazonaws.com "ls -a"
+   spawn sudo ssh -i "acklenavenue.pem" centos@ec2-54-162-255-166.compute-1.amazonaws.com 'ls -a'
    expect Are you sure you want to continue connecting (yes/no)? { send yes\n }
 "
 buildkite-agent artifact upload "*.zip"
