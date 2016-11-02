e003688edd911b6ec870892db627033fc79ff8f4
Camilo Rivera <camilo@riverapineda.com>
2016-11-02 00:13:22 -0600

    testing another aprouch

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -10,5 +10,12 @@ gulp zip-app
 cd zip
 sudo cp ~/acklenavenue.pem acklenavenue.pem
 sudo chmod 400 acklenavenue.pem
-expect ./.buildkite/devDeploy.sh | tee -a bk-pipeline.log
+expect -c "
+   set timeout 1
+   spawn scp -i "acklenavenue.pem" indigo-backend-develop.zip centos@ec2-54-162-255-166.compute-1.amazonaws.com:/home/centos/
+   expect yes/no { send yes\r ; exp_continue }
+   expect 100%
+   sleep 1
+   exit
+"
 #buildkite-agent artifact upload "*.zip"
