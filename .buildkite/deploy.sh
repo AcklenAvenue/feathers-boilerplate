23f750009b4f19b500d750b62b3ebdabc019ac24
Camilo Rivera <camilo@riverapineda.com>
2016-11-06 08:58:36 -0600

    fix question

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -11,7 +11,7 @@ sudo cp ~/acklenavenue.pem acklenavenue.pem
 sudo chmod 400 acklenavenue.pem
 expect -c "
    spawn sudo scp -i "acklenavenue.pem" indigo-backend-$ENVIRONMENT.zip centos@indigo-backend-dev.acklenavenueclient.com:/home/centos/
-   expect Are you sure you want to continue connecting (yes/no)? { send yes\n }
+   expect yes/no { send yes\r ; exp_continue }
 "
 sudo ssh -i "acklenavenue.pem" indigo-backend-dev.acklenavenueclient.com 'unzip -o indigo-backend-$ENVIRONMENT.zip -d /home/centos/builds'
 sudo ssh -i "acklenavenue.pem" indigo-backend-dev.acklenavenueclient.com 'npm install /home/centos/builds'
