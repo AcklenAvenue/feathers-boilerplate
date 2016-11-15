be447f66e16ada6fafe9f596269f19c99df75f6f
Camilo Rivera <camilo@riverapineda.com>
2016-11-15 13:37:07 -0600

    test ssh with password

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -2,13 +2,31 @@ const gulp = require('gulp');
 const runSequence = require('run-sequence');
 const scp = require('gulp-scp2');
 const environment = process.env.ENVIRONMENT || 'dev';
+var ENVPASSWORD = "";
+var ENVUSER = "";
+var ENVHOST = "";
+if(environment === "dev"){
+  ENVPASSWORD = process.env.ENVPASSWORD_DEV;
+  ENVUSER = process.env.ENVUSER_DEV;
+  ENVHOST = process.env.ENVHOST_DEV;
+}
+if(environment === "staging"){
+  ENVPASSWORD = process.env.ENVPASSWORD_STA;
+  ENVUSER = process.env.ENVUSER_STA;
+  ENVHOST = process.env.ENVHOST_STA;
+}
+if(environment === "production"){
+  ENVPASSWORD = process.env.ENVPASSWORD_PROD;
+  ENVUSER = process.env.ENVUSER_PROD;
+  ENVHOST = process.env.ENVHOST_PROD;
+}
 
 gulp.task('deploy', function() {
     return gulp.src('zip/*.zip')
       .pipe(scp({
-        host: 'indigo-backend-dev.acklenavenueclient.com',
-        username: 'centos',
-        publicKey: 'acklenavenue.pem',
+        host: ENVHOST,
+        username: ENVUSER,
+        password: ENVPASSWORD,
         dest: '/home/centos/',
         watch: function(client) {
           client.on('write', function(o) {
