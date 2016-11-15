b54caf65f728fd2c8eda17e630a2d2f9e1c44158
Camilo Rivera <camilo@riverapineda.com>
2016-11-15 12:44:49 -0600

    add log

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -7,6 +7,8 @@ nvm use default
 echo "--- Deploy to $BUILDKITE_BRANCH"
 sudo cp ~/acklenavenue.pem acklenavenue.pem
 sudo chmod 400 acklenavenue.pem
+pwd
+ls -la
 gulp deploy | tee -a bk-pipeline.log
 sudo ssh -i "acklenavenue.pem" centos@indigo-backend-dev.acklenavenueclient.com "unzip -o indigo-backend-$ENVIRONMENT.zip -d /home/centos/builds"
 sudo ssh -i "acklenavenue.pem" centos@indigo-backend-dev.acklenavenueclient.com 'cd builds && npm install'
