e02b479ac663e5b9d30ccbf03e395df47f3c6aae
Camilo Rivera <camilo@riverapineda.com>
2016-11-15 12:40:28 -0600

    fix location

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -5,10 +5,9 @@ echo "--- Set Node Version"
 . "$NVM_DIR/nvm.sh"
 nvm use default
 echo "--- Deploy to $BUILDKITE_BRANCH"
-cd zip
 sudo cp ~/acklenavenue.pem acklenavenue.pem
 sudo chmod 400 acklenavenue.pem
 gulp deploy | tee -a bk-pipeline.log
 sudo ssh -i "acklenavenue.pem" centos@indigo-backend-dev.acklenavenueclient.com "unzip -o indigo-backend-$ENVIRONMENT.zip -d /home/centos/builds"
 sudo ssh -i "acklenavenue.pem" centos@indigo-backend-dev.acklenavenueclient.com 'cd builds && npm install'
-buildkite-agent artifact upload "*.zip"
+buildkite-agent artifact upload "zip/*.zip"
