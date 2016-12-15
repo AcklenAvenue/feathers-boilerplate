00fb3f3b3e8610a9764dbcdd661673ecb97c8711
Ferenck Vargas <ferenck@acklenavenue.com>
2016-12-15 15:28:33 -0600

    Generating sequence numbers for new addresses

--- src/services/customer/hooks/updateAddresses.js
+++ src/services/customer/hooks/updateAddresses.js
@@ -16,12 +16,19 @@ module.exports = function (options) {
     if (hook.params.updateParams && hook.params.updateParams.updateAddresses) {
       const shippingAddresses = hook.app.service('/shippingAddresses');
       const billingAddresses = hook.app.service('/billingAddresses');
+
+      var maxSequence = hook.data.shippingAddresses.reduce((max, address) => {
+        return (max < (address.assistSequence || -1 )) ? address.assistSequence : max;
+      }, 0);
+
       const updatePromises = hook.data.shippingAddresses.map(address => {
         if (address.id !== 0) {
           console.log('update');
           return shippingAddresses.update(address.id, address);
         } else {
           console.log('create');
+          maxSequence++;
+          address.assistSequence = maxSequence;
           return shippingAddresses.create(address);
         }
       });
